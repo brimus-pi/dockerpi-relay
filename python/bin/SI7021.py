cb655a4b89089764798cd200e86ee8d5842e3eff
brimus-pi <54755282+brimus-pi@users.noreply.github.com>
2019-09-10 12:43:02 -0400

    Update SI7021.py

--- python/bin/SI7021.py
+++ python/bin/SI7021.py
@@ -8,6 +8,7 @@ import board
 import busio
 import adafruit_si7021
 import RPi.GPIO as GPIO
+import gpiozero
 
 config = configparser.ConfigParser()
 config.read('/home/pi/etc/si7021.cfg')
@@ -18,6 +19,9 @@ LOGLEVEL = config['LOGGING']['LEVEL']
 LOGNAME = config['LOGGING']['FILE']
 i2c = busio.I2C(board.SCL, board.SDA)
 sensor = adafruit_si7021.SI7021(i2c)
+cpu = gpiozero.CPUTemperature()
+cpu_c = cpu.temperature
+cpu_f = 9 / 5 * cpu_c + 32
 
 ## Add logging information
 logging.basicConfig(filename=LOGNAME, level=LOGLEVEL)
@@ -38,11 +42,15 @@ def getReading():
     H = sensor.relative_humidity
     print("{} -- Temp F is {}".format(time.asctime(), round(F,1)))
     print("{} -- Temp C is {}".format(time.asctime(), round(C, 1)))
+    print("{} -- CPU Temp F is {}".format(time.asctime(), round(cpu_f, 1)))
+    print("{} -- CPU Temp C is {}".format(time.asctime(), round(cpu_c, 1)))
     print("{} -- Humidity is {}".format(time.asctime(), round(H, 1)))
     logging.info("==============================================")
     logging.info(" ")
     logging.info("{} -- Temp F is {}".format(time.asctime(), round(F, 1)))
     logging.info("{} -- Temp C is {}".format(time.asctime(), round(C, 1)))
+    logging.info("{} -- CPU Temp F is {}".format(time.asctime(), round(cpu_f, 1)))
+    logging.info("{} -- CPU Temp C is {}".format(time.asctime(), round(cpu_c, 1)))
     logging.info("{} -- Humidity is {}".format(time.asctime(), round(H, 1)))
     logging.info(" ")
 
